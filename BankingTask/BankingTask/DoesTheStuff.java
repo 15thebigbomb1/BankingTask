68cc960a2e353847e681513092ef40bd11f6faad
15thebigbomb1 <166461579+15thebigbomb1@users.noreply.github.com>
2025-03-30 23:17:17 +1300

    Add files via upload
    I have Changed the code in the check balance class so that if you dont input the right name you can choose to go back to the menu or re enter, (note calling the menu is not working at the moment)

---
+++ BankingTask/BankingTask/DoesTheStuff.java
@@ -0,0 +1,109 @@
+/**
+ * Write a description of class DoesTheStuff here.
+ *
+ * @author (Gabriel Gibson)
+ * @version (30/03/25)
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+public class DoesTheStuff
+{
+    private String name;
+    private int account;
+    private String address;
+    private String accountType;
+    private int balance;
+    // all the private info of the users
+    private String[] values;
+    
+    int MAXLINES = 20;
+    final int VALUESPERLINE= 5;
+    private Scanner kb = new Scanner(System.in);
+    ArrayList<BankingTask> info = new ArrayList<BankingTask>();
+    int linecount = 0;
+    File userInfoList = new File("TestAccounts.csv");
+    private String[][] allLinesAllElements;
+    // defining 2d arrays and loops for file checking, and also defing where the values will be
+    // kept with the 2d array, "allLinesAllElements".
+    public void DefineFiles(){
+        String CSVlines[] = new String[MAXLINES];
+        ArrayList<String[]> tempList = new ArrayList<>();
+        //defines the temporary list that will be used to add my values to the 2d array
+        try {
+            Scanner reader = new Scanner(userInfoList); // open the file with the Scanner
+            // Read in the file, stop at file end or if we read too many lines
+            while (reader.hasNextLine()  && linecount < MAXLINES){
+                String line=reader.nextLine();                  
+                CSVlines[linecount]=line;
+                //System.out.println(CSVlines[linecount]);
+                linecount++;
+
+            }
+            // defines The File 
+            for (int i = 0; i<linecount;i++){
+                values = CSVlines[i].split(",");
+                tempList.add(values);
+            }
+            //Separates the arry at every comma of the csv line
+            //Adds to a tempoary array that then gets converted to a 2d array
+            //This 2d arrays first value is the name and the second values are all the info
+            allLinesAllElements = tempList.toArray(new String[tempList.size()][]);
+            //defines the templists values and adds it to the 2d array, with the first
+            // variable being the amount of rows, and the second being the info name, adress etc.
+            
+                        
+
+        } catch (IOException e) {System.out.println(e);}
+    }
+
+    public void CreateAccount(){
+        System.out.println("hello 1");
+    }
+
+    public void CloseAccount(){
+        System.out.println("hello! 2");
+    }
+
+    public void CheckBalance(){
+        boolean nameCheck = true;
+        int b = 0;
+        // int value for the while loop that chec
+        System.out.println("enter in your name to check your balance");
+        
+        name = kb.nextLine();
+        // the name the user will input to find there account
+        
+        while (nameCheck == true){
+            if (name.equals(allLinesAllElements[b][0])){
+                System.out.println(allLinesAllElements[b][4]);
+                // prints out the balance of the name chosen as the balances will always
+                // be the fifth value in the array
+                nameCheck = false;
+                // turns off the loop
+            } else{
+                b++;
+                if (b == allLinesAllElements.length){
+                    System.out.println("Name not identifed");
+                    nameCheck = false;
+                    //turns off the loop if the name is not found
+                }
+            }
+        }
+        
+        System.out.println("Do you want to check/recheck your balance, Type yes to do so, otherwise type no to go back to the menu");
+        String balanceChoice= kb.nextLine().toUpperCase();
+        if (balanceChoice.equals("YES") || balanceChoice.equals("Y") || balanceChoice.equals("YEP")){
+            CheckBalance();
+        } else if (balanceChoice.equals("NO") || balanceChoice.equals("N") || balanceChoice.equals("NAH")){
+           System.out.println("Try Again Later");
+        }
+        
+    }
+
+    public void DepositOrWithdrawl(){
+        System.out.println("hello! 4");
+    }
+}
